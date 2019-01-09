ca4efc9f049bc1feb966c92827fd7eddb0af4429
christopher crew baker <chriscrewbaker@gmail.com>
2019-01-08 23:04:55 -0500

    should fail on travis

--- tests/testthat/test-abstract.R
+++ tests/testthat/test-abstract.R
@@ -14,7 +14,7 @@ test_that("ma_abstract works as expected", {
 
   vcr::use_cassette("ma_abstract_no_NAs", {
     res <- ma_abstract("Id=2142076420")
-    expect_false(grepl("\\bNA\\b", res$abstract))
+    expect_true(grepl("\\bNA\\b", res$abstract))
   }, preserve_exact_body_bytes = TRUE)
 
 })
